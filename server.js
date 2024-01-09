fe365131dd0dc159eb10a25fd4c454738a1dc87e
Derin DeNio <derindenio@gmail.com>
2024-01-09 13:20:22 -0700

    Added function to reduce redundancy

--- server.js
+++ server.js
@@ -6,34 +6,43 @@ const querystring = require('querystring');
 const figlet = require('figlet')
 
 const server = http.createServer((req, res) => {
+
+  const readWrite = (file, contentType) => {
+    fs.readFile(file, function (err, data) {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.write(data);
+      res.end();
+    });
+  }
+
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
   if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
   }
   else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('otherpage.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
   }
   else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('otherotherpage.html', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
   }
   else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+    if ('student' in params) {
+      if (params['student'] == 'leon') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const objToJson = {
           name: "leon",
           status: "Boss Man",
@@ -41,8 +50,8 @@ const server = http.createServer((req, res) => {
         }
         res.end(JSON.stringify(objToJson));
       }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+      else if (params['student'] != 'leon') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
         const objToJson = {
           name: "unknown",
           status: "unknown",
@@ -52,23 +61,23 @@ const server = http.createServer((req, res) => {
       }//student != leon
     }//student if
   }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
+  else if (page == '/css/style.css') {
+    fs.readFile('css/style.css', function (err, data) {
       res.write(data);
       res.end();
     });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
+  } else if (page == '/js/main.js') {
+    fs.readFile('js/main.js', function (err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
       res.write(data);
       res.end();
     });
-  }else{
-    figlet('404!!', function(err, data) {
+  } else {
+    figlet('404!!', function (err, data) {
       if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
       }
       res.write(data);
       res.end();
